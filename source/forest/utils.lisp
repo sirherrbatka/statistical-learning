90846e2ffc4de404ee406f038a8722d071b00fd6
Marek Kochanowicz <sirherrbatka@gmail.com>
2020-05-11 13:25:06 +0200

    Avoid rehashing in select-random-indexes.

--- source/forest/utils.lisp
+++ source/forest/utils.lisp
@@ -21,14 +21,13 @@
 (-> select-random-indexes (fixnum fixnum) (simple-array fixnum (*)))
 (defun select-random-indexes (selected-count total-count)
   (declare (optimize (speed 3) (safety 0)))
-  (let ((table (make-hash-table))
-        (result (make-array (min selected-count total-count)
-                            :element-type 'fixnum)))
-    (declare (dynamic-extent table))
+  (let* ((table (make-hash-table :size total-count))
+         (limit (min selected-count total-count))
+         (result (make-array limit :element-type 'fixnum)))
     (iterate
       (declare (type fixnum i random-position value next-value
                      lower-bound))
-      (for i from 0 below selected-count)
+      (for i from 0 below limit)
       (for lower-bound = (- total-count i))
       (for random-position = (+ i (random lower-bound)))
       (for value = (ensure (gethash i table) i))
