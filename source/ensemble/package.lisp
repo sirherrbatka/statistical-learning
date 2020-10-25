000bcc61bdd8fe19b890ea3dadfddbdf73f3069b
Marek Kochanowicz <sirherrbatka@gmail.com>
2020-10-25 16:36:22 +0100

    Export new symbols from the ensemble package.

--- source/ensemble/package.lisp
+++ source/ensemble/package.lisp
@@ -5,18 +5,27 @@
   (:use #:cl #:statistical-learning.aux-package)
   (:nicknames #:sl.ensemble)
   (:export
+   #:*state*
+   #:after-tree-fitting
+   #:after-tree-fitting/proxy
+   #:assign-leafs
+   #:assigned-leafs
    #:dynamic-weights-calculator
    #:ensemble-model
    #:gradient-boost-ensemble
    #:gradient-boost-ensemble-model
+   #:indexes
    #:leafs
+   #:leafs-assigned-p
+   #:make-tree-training-state
    #:make-tree-training-state
    #:make-tree-training-state/proxy
-   #:*state*
+   #:make-tree-training-state/proxy
    #:random-forest
    #:random-forest-model
    #:static-weights-calculator
    #:tree-batch-size
    #:tree-sample-rate
    #:trees
+   #:trees-view
    #:update-weights))
